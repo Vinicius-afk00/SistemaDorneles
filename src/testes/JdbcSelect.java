52462b3cac4d8a82d6ff030dd924fa2afe124c29
Vinicius-afk00 <viniperesdorneles08@gmail.com>
2026-05-04 12:17:41 -0400

    Tabelas interligadas e funcionando

--- src/testes/JdbcSelect.java
+++ src/testes/JdbcSelect.java
@@ -1,23 +1,22 @@
-    /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package testes;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author u08538003160
  */
-public class JdbcCrud {
+public class JdbcSelect {
     public static void main(String[] args) {
         try {            
             Class.forName("com.mysql.jdbc.Driver");
@@ -27,24 +26,14 @@ public class JdbcCrud {
                     password = "marcos_vilhanueva";
                     Connection cnt;
                     cnt = DriverManager.getConnection(url, user, password);
-//                    Statement stm = cnt.createStatement();
-//                    String slq = "insert into "
-//                            + "mpv_usuarios(mpv_idusuarios,mpv_nome, mpv_apelido, mpv_cpf) "
-//                            + "values(103, 'vinicius', 'vpd', '646887456843')";
-//                    stm.executeUpdate(slq);
-//                    String sql = "insert into "
-//                            + "mpv_usuarios(mpv_idusuarios,mpv_nome, mpv_apelido, mpv_cpf)"
-//                            + " values (?, ?, ?, ?)";
-//                    String sql = "update mpv_usuarios"
-//                            + " set mpv_nome = ?, mpv_apelido= ?, mpv_cpf= ?"
-//                            + " where mpv_idusuarios= ?";
-                      String sql = "delete from mpv_usuarios where mpv_idusuarios = ?";
+                      String sql = "select * from mpv_usuarios";
                     PreparedStatement pst = cnt.prepareStatement(sql);
                     pst.setInt(1, 503);
-//                    pst.setString(1, "Vinicius Peres Dorneles");
-//                    pst.setString(2, "vpd");
-//                    pst.setString(3, "56893262666");
-                    pst.executeUpdate();
+                    pst.setString(1, "Vinicius Peres Dorneles");
+                    pst.setString(2, "vpd");
+                    pst.setString(3, "56893262666");
+                    ResultSet rs = pst.executeQuery();
+                    rs.next();
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(JdbcCrud.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
